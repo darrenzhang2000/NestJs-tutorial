02cb62cefadac6002e56e51b6226908209a1c991
Darren <darrenzhang200027@gmail.com>
2022-01-11 12:55:58 -0500

    added mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot(
+    'mongodb+srv://testuser:testuser123@cluster0.jhnrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
